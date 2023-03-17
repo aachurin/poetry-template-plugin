cb66cbe575b4b5ddf3c2a07e33a13c1c6467debe
aachurin <aachurin@gmail.com>
2023-03-17 23:34:32 +0300

    # v0.1.8

--- poetry_template_plugin/plugin.py
+++ poetry_template_plugin/plugin.py
@@ -12,8 +12,9 @@ import subprocess
 import tempfile
 from functools import cached_property
 from pathlib import Path
-from typing import Any, Callable, Dict, List, Match
+from typing import Any, Callable, Dict, List, Match, Tuple
 
+import merge3
 import tomlkit
 from cleo.application import Application
 from cleo.commands.command import Command
@@ -76,6 +77,23 @@ class TemplateBaseCommand(Command):
 
     def print(self, message: str) -> None:
         self.io.write_line(message)
+
+    def print_file(self, path: Path, flag: str, ignore: bool = False, conflict: bool = False, **_kwargs: Any) -> None:
+        if ignore:
+            self.print(f"<fg=cyan>\u24e7  {path}</>")
+            return
+        extra = "[conflict]" if conflict else ""
+        if flag == "initial":
+            color, icon = "yellow", "\u24d8"
+        elif flag == "optional":
+            color, icon = "blue", "\u24de"
+        elif flag == "protected":
+            color, icon = "magenta", "\u24df"
+        else:
+            color, icon = "gray", "?"
+        if conflict:
+            color = "red"
+        self.print(f"<fg={color}>{icon}  {path} {extra}</>")
 
 
 class TemplateCommand(TemplateBaseCommand):
@@ -152,12 +170,7 @@ class TemplateCommand(TemplateBaseCommand):
                     fn(files, path)
 
     def write_file(self, file: "File") -> bool:
-        if file.is_initial:
-            self.print(f"<fg=yellow>\u24d8  {file.path}</>")
-        elif file.is_optional:
-            self.print(f"<fg=blue>\u24de  {file.path}</>")
-        elif file.is_protected:
-            self.print(f"<fg=magenta>\u24df  {file.path}</>")
+        self.print_file(file.path, flag=file.options["flag"], conflict=file.merged_content.has_conflicts)
         return file.write()
 
     def cmd_set_initial(self, files: Dict[Path, Dict[str, Any]]) -> Callable[[List[str]], None]:
@@ -238,10 +251,16 @@ class TemplateUpdateCommand(TemplateCommand):
         option("update", description="Run poetry update."),
     ]
 
-    def get_suitable_files(self, files: Dict[Path, "File"]) -> Dict[Path, "File"] | None:
-        result = {p: f for p, f in files.items() if not (f.is_ignored or f.is_initial) and (f.is_new or f.is_changed)}
+    def get_update_files(self, state: TemplateState, files: Dict[Path, "File"]) -> "List[Tuple[str, Path | File]]":
+        result: List[Tuple[str, Path | File]] = []
         error = False
-        for file in result.values():
+        for path in sorted(set(state.files).union(files)):
+            if path not in files:
+                result.append(("delete", path))
+                continue
+            file = files[path]
+            if file.is_ignored or file.is_initial:
+                continue
             if file.is_new and file.exists():
                 error = True
                 self.print(f"<fg=yellow>\u2757 file {file.path} already exists</>")
@@ -250,53 +269,79 @@ class TemplateUpdateCommand(TemplateCommand):
                 error = True
                 self.print(f"<fg=yellow>\u2757 {file.path} is not a regular file</>")
                 continue
+            if file.is_new:
+                result.append(("create", file))
+            elif not file.exists():
+                result.append(("restore", file))
+            elif file.is_changed:
+                result.append(("merge", file))
 
         if error:
             raise RuntimeError("Fix errors and try again")
 
-        return {p: f for p, f in result.items() if not (f.is_optional and f.exists())}
+        return result
+
+    def check_conflicts(self, state: TemplateState) -> None:
+        conflicts = [(path, meta) for path, meta in sorted(state.files.items()) if meta.get("conflict")]
+        if conflicts:
+            self.print("<fg=yellow>\u2757 There are unresolved conflicts:</>")
+            for path, meta in conflicts:
+                self.print_file(path, **meta)
+            self.print("")
+            self.print("<fg=yellow>\u2757 Resolve conflicts and tag files using `poetry template fix`</>")
+            raise RuntimeError("Cancelled")
 
     def handle_files(self, state: TemplateState, files: Dict[Path, "File"]) -> None:
         self.print("\U0001f680 Start update...")
-        suitable_files = self.get_suitable_files(files)
-        if not suitable_files:
-            self.print("<fg=yellow>\U0001F610 Nothing to update</>")
-        else:
-            warning = False
-            remove = []
-            for path in set(state.files).union(files):
-                if path not in files:
-                    warning = True
-                    self.print(f"<fg=yellow>\u2757 [-] file <fg=magenta>{path}</> will be deleted</>")
-                    remove.append(path)
-                elif file := suitable_files.get(path):
-                    if file.exists():
-                        if file.is_changed:
-                            warning = True
-                            self.print(f"<fg=yellow>\u2757 [*] file <fg=magenta>{file.path}</> will be overwritten</>")
-                    elif files[path].is_new:
-                        warning = True
-                        self.print(f"<fg=yellow>\u2757 [+] file <fg=magenta>{file.path}</> will be created</>")
-                    else:
-                        warning = True
-                        self.print(f"<fg=yellow>\u2757 [+] file <fg=magenta>{file.path}</> will be restored</>")
+        self.print("")
+        self.check_conflicts(state)
 
-            if warning:
-                self.print("")
-                if self.ask("Are you sure you want to continue? [y/N]", default="N") != "y":
-                    raise RuntimeError("Cancelled")
-                self.print("")
+        remove: List[Path] = []
+        write: List[File] = []
 
+        for what, file in self.get_update_files(state, files):
+            if what == "delete":
+                self.print(f"<fg=yellow>\u2757 [-] <fg=magenta>{file}</> will be deleted</>")
+                assert isinstance(file, Path)
+                remove.append(file)
+                continue
+            assert isinstance(file, File)
+            if what == "create":
+                self.print(f"<fg=yellow>\u2757 [+] <fg=magenta>{file.path}</> will be created</>")
+                write.append(file)
+            elif what == "restore":
+                self.print(f"<fg=yellow>\u2757 [+] <fg=magenta>{file.path}</> will be restored</>")
+                write.append(file)
+            elif what == "merge":
+                if file.unified_diff:
+                    op = "will be merged" if file.is_optional else "will be overwritten"
+                    conflict = "<fg=red>with CONFLICTS</>" if file.merged_content.has_conflicts else ""
+                    self.print(f"<fg=yellow>\u2757 [*] <fg=magenta>{file.path}</> {op} {conflict}</>")
+                    write.append(file)
+
+        if remove or write:
+            self.print("")
+            if self.ask("Are you sure you want to continue? [y/N]", default="N") != "y":
+                raise RuntimeError("Cancelled")
+            self.print("")
+
+        if write:
             self.print("\U0001f680 Write files...")
-            for file in suitable_files.values():
+            for file in write:
                 self.write_file(file)
+            self.print("")
 
-            if remove:
-                self.print("\U0001f680 Remove files...")
-                for p in remove:
-                    self.print(f"<fg=red>\u24e7 {p}</>")
-                    p.unlink(missing_ok=True)
-                    state.files.pop(p)
+        if remove:
+            self.print("\U0001f680 Remove files...")
+            for p in remove:
+                self.print(f"<fg=red>\u24e7  {p}</>")
+                p.unlink(missing_ok=True)
+                path = p.parent
+                while path != Path(".") and not any(path.glob("*")):
+                    path.rmdir()
+                    path = path.parent
+                state.files.pop(p)
+            self.print("")
 
     def done(self) -> None:
         if self.option("update"):
@@ -313,31 +358,31 @@ class TemplateIncomingCommand(TemplateUpdateCommand):
 
     def handle_files(self, state: TemplateState, files: Dict[Path, "File"]) -> None:
         self.print("\U0001f680 Check incoming updates...")
-        suitable_files = self.get_suitable_files(files)
-        if not suitable_files:
-            self.print("<fg=yellow>\U0001F610 Nothing to update</>")
-        else:
-            for path in set(state.files).union(files):
-                if path not in files:
-                    self.print(f"<fg=yellow>\u2757 [-] file <fg=magenta>{path}</> will be deleted</>")
+        self.print("")
+        self.check_conflicts(state)
+        for what, file in self.get_update_files(state, files):
+            if what == "delete":
+                self.print(f"<fg=yellow>\u2757 [-] <fg=magenta>{file}</> will be deleted</>")
+                self.print("=" * 79)
+                continue
+            assert isinstance(file, File)
+            if what == "create":
+                self.print(f"<fg=yellow>\u2757 [+] <fg=magenta>{file.path}</> will be created</>")
+                self.print("=" * 79)
+            elif what == "restore":
+                self.print(f"<fg=yellow>\u2757 [+] <fg=magenta>{file.path}</> will be restored</>")
+                self.print("=" * 79)
+            elif what == "merge":
+                if file.unified_diff:
+                    op = "will be merged" if file.is_optional else "will be overwritten"
+                    conflict = "<fg=red>with CONFLICTS</>" if file.merged_content.has_conflicts else ""
+                    self.print(f"<fg=yellow>\u2757 [*] <fg=magenta>{file.path}</> {op} {conflict}</>")
                     self.print("-" * 79)
-                elif file := suitable_files.get(path):
-                    if file.exists():
-                        if file.is_changed:
-                            self.print(f"<fg=yellow>\u2757 [*] file <fg=magenta>{file.path}</> will be overwritten</>")
-                            self.print_diffs(file.path, file.local_content, file.get_write_content())  # type:ignore
-                            self.print("-" * 79)
-                    elif files[path].is_new:
-                        self.print(f"<fg=yellow>\u2757 [+] file <fg=magenta>{file.path}</> will be created</>")
-                        self.print("-" * 79)
-                    else:
-                        self.print(f"<fg=yellow>\u2757 [+] file <fg=magenta>{file.path}</> will be restored</>")
-                        self.print("-" * 79)
+                    self.print_diff(file.unified_diff)
+                    self.print("=" * 79)
 
-    def print_diffs(self, path: Path, file_a: bytes, file_b: bytes) -> None:
-        lines_a = file_a.decode("utf-8").splitlines(keepends=True)
-        lines_b = file_b.decode("utf-8").splitlines(keepends=True)
-        for line in difflib.unified_diff(lines_a, lines_b, fromfile=str(path), tofile=str(path), n=1):
+    def print_diff(self, content: List[str]) -> None:
+        for line in content:
             line = line.rstrip()
             line = self.io.output.formatter.escape(line)
             if line.startswith("-"):
@@ -358,15 +403,8 @@ class TemplateListCommand(TemplateBaseCommand):
     options = []
 
     def handle_command(self, state: TemplateState, target_dir: Path) -> None:
-        for path, meta in state.files.items():
-            if meta.get("ignore"):
-                self.print(f"<fg=cyan>\u24e7  {path}</>")
-            if meta.get("flag") == "initial":
-                self.print(f"<fg=yellow>\u24d8  {path}</>")
-            elif meta.get("flag") == "optional":
-                self.print(f"<fg=blue>\u24de  {path}</>")
-            elif meta.get("flag") == "protected":
-                self.print(f"<fg=magenta>\u24df  {path}</>")
+        for path, meta in sorted(state.files.items()):
+            self.print_file(path, **meta)
 
 
 class TemplateLinkCommand(TemplateBaseCommand):
@@ -389,6 +427,23 @@ class TemplateLinkCommand(TemplateBaseCommand):
             state.files[path].update({"ignore": False})
 
 
+class TemplateFixCommand(TemplateBaseCommand):
+    name = "template fix"
+    description = "Fix resolved conflict."
+    arguments = [
+        argument("file", "File to fix.", multiple=False),
+    ]
+
+    def handle_command(self, state: TemplateState, target_dir: Path) -> None:
+        path = Path(self.argument("file"))
+        if path not in state.files:
+            raise RuntimeError("The given file does not exist")
+        if not state.files[path].get("conflict"):
+            raise RuntimeError("The given file has no conflicts")
+        state.files[path]["conflict"] = False
+        state.files[path]["lock"] = FILES.get(path.suffix, File).get_hash(path.read_bytes())
+
+
 class TemplatePlugin(ApplicationPlugin):
     def activate(self, application: Application, *args: Any, **kwargs: Any) -> None:
         application.command_loader.register_factory("template init", TemplateInitCommand)  # type:ignore
@@ -396,6 +451,11 @@ class TemplatePlugin(ApplicationPlugin):
         application.command_loader.register_factory("template incoming", TemplateIncomingCommand)  # type:ignore
         application.command_loader.register_factory("template list", TemplateListCommand)  # type:ignore
         application.command_loader.register_factory("template link", TemplateLinkCommand)  # type:ignore
+        application.command_loader.register_factory("template fix", TemplateFixCommand)  # type:ignore
+
+
+class MergedContent(bytes):
+    has_conflicts: bool = False
 
 
 class File:
@@ -436,17 +496,38 @@ class File:
 
     @cached_property
     def is_template_changed(self) -> bool:
-        return self.get_hash(self.content) != self.get_hash(self.state.get("content"))
+        return self.get_hash(self.content) != self.get_hash(self.base_content)
 
     @cached_property
     def is_changed(self) -> bool:
         return self.is_locally_changed or self.is_template_changed
 
     @cached_property
-    def local_content(self) -> bytes | None:
+    def base_content(self) -> bytes:
+        return self.state.get("content") or b""
+
+    @cached_property
+    def local_content(self) -> bytes:
         if self.path.is_file():
             return self.path.read_bytes()
-        return None
+        return b""
+
+    @cached_property
+    def merged_content(self) -> MergedContent:
+        if not self.path.exists() or self.is_new:
+            return MergedContent(self.content)
+        return self.get_merged_content()
+
+    @cached_property
+    def unified_diff(self) -> List[str]:
+        return list(
+            difflib.unified_diff(
+                self.local_content.decode("utf-8").splitlines(keepends=True),
+                self.merged_content.decode("utf-8").splitlines(keepends=True),
+                fromfile=str(self.path),
+                tofile=str(self.path),
+            )
+        )
 
     def exists(self) -> bool:
         return self.path.exists()
@@ -454,62 +535,71 @@ class File:
     def is_file(self) -> bool:
         return self.path.is_file()
 
-    def _write(self, content: bytes) -> bool:
-        if not self.path.parent.exists():
-            os.makedirs(self.path.parent)
-        if self.path.exists():
-            self.path.chmod(0o644)
-        self.path.write_bytes(content)
-        if self.is_protected and self.is_readonly:
-            self.path.chmod(0o444 | self.xmode)
-        else:
-            self.path.chmod(0o644 | self.xmode)
-        self.state.update({"content": self.content, "lock": self.get_hash(content), "flag": self.options["flag"]})
-        return True
-
-    def get_write_content(self) -> bytes | None:
-        if not self.path.exists() or not self.state:
-            return self.content
-        if self.is_changed:
-            return self.merge(self.state.get("content"), self.content, self.local_content)  # type:ignore
-        return None
-
     def write(self) -> bool:
-        content = self.get_write_content()
-        if content is not None:
-            return self._write(content)
+        if self.is_changed or self.is_new:
+            if not self.path.parent.exists():
+                os.makedirs(self.path.parent)
+            if self.path.exists():
+                self.path.chmod(0o644)
+            self.path.write_bytes(self.merged_content)
+            if self.is_protected and self.is_readonly:
+                self.path.chmod(0o444 | self.xmode)
+            else:
+                self.path.chmod(0o644 | self.xmode)
+            state = {
+                "content": self.content,
+                "lock": self.get_hash(self.merged_content),
+                "flag": self.options["flag"],
+                "conflict": self.merged_content.has_conflicts,
+            }
+            self.state.update(state)
+            return True
         return False
 
     @classmethod
-    def get_hash(cls, data: bytes | None) -> str | None:
-        return hashlib.md5(data).hexdigest() if data is not None else None
+    def get_hash(cls, data: bytes) -> str:
+        return hashlib.md5(data).hexdigest()
 
-    @classmethod
-    def merge(cls, prev_content: bytes | None, new_content: bytes, cur_content: bytes) -> bytes:
-        return new_content
+    def get_merged_content(self) -> MergedContent:
+        if self.is_optional:
+            m3 = merge3.Merge3(
+                self.base_content.decode("utf-8").splitlines(keepends=True),
+                self.local_content.decode("utf-8").splitlines(keepends=True),
+                self.content.decode("utf-8").splitlines(keepends=True),
+            )
+            result = MergedContent(
+                "".join(
+                    m3.merge_lines(
+                        name_a=f"LOCAL: {self.path}",
+                        name_b=f"REMOTE: {self.path}",
+                        name_base=f"BASE: {self.path}",
+                    )
+                ).encode("utf-8")
+            )
+            result.has_conflicts = any(x[0] == "conflict" for x in m3.merge_regions())
+            return result
+
+        return MergedContent(self.content)
 
 
 class PyFile(File):
     @classmethod
-    def get_hash(cls, data: bytes | None) -> str | None:
-        if data is None:
-            return None
+    def get_hash(cls, data: bytes) -> str:
         try:
             file = ast.unparse(ast.parse(data)).encode("utf-8")
         except SyntaxError:
-            return None
+            file = data
         return hashlib.md5(file).hexdigest()
 
 
 class TomlFile(File):
-    @classmethod
-    def merge(cls, prev_content: bytes | None, new_content: bytes, cur_content: bytes) -> bytes:
-        result = cls._merge(cls._load(prev_content), cls._load(new_content), cls._load(cur_content))
-        return tomlkit.dumps(result).encode("utf-8")
+    def get_merged_content(self) -> MergedContent:
+        result = self._merge(self._load(self.base_content), self._load(self.content), self._load(self.local_content))
+        return MergedContent(tomlkit.dumps(result).encode("utf-8"))
 
     @classmethod
-    def _load(cls, data: bytes | None) -> Any:
-        return cls._normalize_doc(tomlkit.loads(data)) if data else None
+    def _load(cls, data: bytes) -> Any:
+        return cls._normalize_doc(tomlkit.loads(data)) if data.strip() else None
 
     @classmethod
     def _normalize_doc(cls, data: tomlkit.TOMLDocument) -> tomlkit.TOMLDocument:
